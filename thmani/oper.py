c44d7bc65c1ba72f3f769fbe672183916c51e847
BerryDenHartog <berrydenhartog@mailbox.org>
2020-01-30 20:31:14 +0100

    updates

--- thmani/oper.py
+++ thmani/oper.py
@@ -5,7 +5,8 @@ import time
 
 # connect to rabbitmq message server
 credentials = pika.PlainCredentials('guest', 'guest')
-parameters = pika.ConnectionParameters(host='rabbitmq',port=5672,credentials=credentials)
+parameters = pika.ConnectionParameters(host='rabbitmq',port=5672,credentials=credentials,connection_attempts=5,retry_delay=5)
+connection = None
 try:
     connection = pika.BlockingConnection(parameters)
 except Exception as e:
@@ -16,7 +17,6 @@ channel = connection.channel()
 
 # check if queue exist. else create
 channel.queue_declare(queue='hello')
-
 
 trace={
     VERSION: 1.0,
@@ -32,12 +32,13 @@ trace={
     ]   
 }
 
-# push a message to rabbitmq
-for i in range(0,100)
+# initialization phase
+
+# execution phase
+for i in range(0,100):
     trace[TRACEHEADER][SEQ]=i
-    channel.basic_publish(exchange='test', routing_key='test',
-                      body=json.dumps(trace))
+    channel.basic_publish(exchange='test', routing_key='test', body=json.dumps(trace))
     time.sleep(1)
 
-# close connection
+# finalization phase
 connection.close()
